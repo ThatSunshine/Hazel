e1c45278cbd87366d6a773d81cdba17c864f45c2
ThatSunshine <1046474845@qq.com>
2023-07-17 21:46:01 +0800

    add entry point

---
+++ Hazel/src/Hazel/EntryPoint.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Core.h"
+
+
+#ifdef HZ_PLATFORM_WINDOWS
+
+extern Hazel::Application* Hazel::CreateApplication();
+
+int main(int argc, char** argav)
+{
+	printf("Start Hazel Engine!");
+	auto app = Hazel::CreateApplication();
+	app->Run();
+	delete app;
+}
+
+#endif
